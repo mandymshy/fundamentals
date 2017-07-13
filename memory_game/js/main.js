eccfbe5677aeaf6bf59b5cb271530394f7befd50
Your Name <mandymakweni1@gmail.com>
2017-07-13 13:39:56 +0200

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,14 @@
+ console.log("Up and running!");
+  var cardOne = " queen ";
+    console.log("User flipped " + cardOne);
+
+  var cardtwo = "king";
+    console.log("User flipped " + cardtwo);
+
+  var cardthree = " queen ";
+    console.log("User flipped " + cardthree);
+
+  var cardfour = "king";
+    console.log("User flipped " + cardfour);
+   
+  
