2d664c962f673424f732bb718e29e0cbf86986b9
Your Name <mandymakweni1@gmail.com>
2017-07-17 15:04:19 +0200

    create game logic

---
+++ js/main.js
@@ -0,0 +1,11 @@
+ console.log("Up and running!");
+console.log("User flipped queen");
+console.log("User flipped king");
+var cardOne = ("queen");
+var cardTwo = ("queen");
+var cardThree = ("queen");
+var cardFour = ("queen");
+
+  console.log("User flipped " + cardOne);
+
+
