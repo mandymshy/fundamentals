ad3d5e706c7f4171049c0c0d32e74e2d9de2d8f8
Your Name <mandymakweni1@gmail.com>
2017-07-27 13:52:27 +0200

    game

---
+++ js/main.js
@@ -0,0 +1,23 @@
+ console.log("Up and running!");
+<<<<<<< HEAD
+console.log("User flipped queen");
+console.log("User flipped king");
+var cardOne = ("queen");
+var cardTwo = ("queen");
+var cardThree = ("queen");
+var cardFour = ("queen");
+
+  console.log("User flipped " + cardOne);
+
+
+=======
+
+  var cardOne = "king";
+  var cardtwo = "king";
+  var cardthree = "queen";
+  var cardfour = "queen";
+
+    console.log("User flipped " + cardOne);
+
+
+ 
